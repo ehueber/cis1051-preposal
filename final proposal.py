e04736247e9ffdc5465889e28fc62877084128e2
ehueber <129543159+ehueber@users.noreply.github.com>
2023-04-04 21:07:30 -0400

    Add files via upload

---
+++ final proposal.py
@@ -0,0 +1,42 @@
+# Proposal
+
+## What will (likely) be the title of your project?
+
+The ultimate password generator
+
+## In just a sentence or two, summarize your project. (E.g., "A website that lets you buy and sell stocks.")
+
+A password checker that evalutes the stregth of your chosen password and offers suggestions for new ones if not strong enough.  
+
+## In a paragraph or more, detail your project. What will your software do? What features will it have? How will it be executed?
+
+Our project will involve two elements. The first the user will have to input a password our python software will generate answer as to where that password is accpeted.
+If accepted it will tell you how strong it is maybe with a metered graphic. If it does not meet conditions or not strong enough it will promote the user with a random password.
+If they do not want a random password they can choose to have it generate a password that is similar but acceptable to their input. 
+
+## If planning to combine 1051's final project with another course's final project, with which other course? And which aspect(s) of your proposed project would relate to 1051, and which aspect(s) would relate to the other course?
+
+TODO, if applicable
+
+## If planning to collaborate with 1 or 2 classmates for the final project, list their names, email addresses, and the names of their assigned TAs below.
+
+Matt Sivo, 
+## In the world of software, most everything takes longer to implement than you expect. And so it's not uncommon to accomplish less in a fixed amount of time than you hope.
+
+### In a sentence (or list of features), define a GOOD outcome for your final project. I.e., what WILL you accomplish no matter what?
+
+We will defintely have a working generator and a condition that says when note enough letters or numbers.
+
+### In a sentence (or list of features), define a BETTER outcome for your final project. I.e., what do you THINK you can accomplish before the final project's deadline?
+
+I hope we can creater a meter for how secure their password is along with a suggestion for a similar password if theirs is not right.
+
+### In a sentence (or list of features), define a BEST outcome for your final project. I.e., what do you HOPE to accomplish before the final project's deadline?
+
+I hope to accomplish maybe a meter that shows how strong, generate a random one if promoted, relpy is password is not up to standards, and offer a similar suggestion to their input. 
+
+## In a paragraph or more, outline your next steps. What new skills will you need to acquire? What topics will you need to research? If working with one of two classmates, who will do what?
+
+We will need to research generators and if random will be a good choice. Next we will have to work on creating conditions for inputs and create the boundries of what needed to be inputed.
+Then work on how strong it is and maybe make meter. Create list of common passwords and have them be unusable. Lastly work on creating a code that will give you suggetsions.
+Me and matt will do most of the work together and delgate small tasks when the time comes.
